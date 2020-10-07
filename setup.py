dd3c2a13cee26c614dd5f44eb2654be19f554251
Daniel <borzecki.daniel@gmail.com>
2020-10-07 21:13:59 +0200

    bump requirements and package versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='reviewio',
-    version='0.2',
+    version='0.2.1',
     author='borzeckid',
     author_email='borzecki.daniel@gmail.com',
     description='Display statistics of pull request reviewers for your project',
